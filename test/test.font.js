4012fd98cf11c19cb634a919126de01fce829fef
夏明星 <xiamingxing@360.cn>
2016-09-13 16:49:30 +0800

    增加系统检验

--- test/test.font.js
+++ test/test.font.js
@@ -4,19 +4,19 @@ import {expect} from "chai"
 import {resolveSystemFontPath, contain, all, is} from "../src/font";
 
 describe("font 库测试", () => {
-    before(function() {
+    before(function () {
         // 在本区块的所有测试用例之前执行
     });
 
-    after(function() {
+    after(function () {
         // 在本区块的所有测试用例之后执行
     });
 
-    beforeEach(function() {
+    beforeEach(function () {
         // 在本区块的每个测试用例之前执行
     });
 
-    afterEach(function() {
+    afterEach(function () {
         // 在本区块的每个测试用例之后执行
     });
 
@@ -24,9 +24,11 @@ describe("font 库测试", () => {
         expect(all().length).to.be.above(0)
     });
 
-    it("华文黑体的路径是", () => {
-        expect(resolveSystemFontPath("华文黑体")[0]).to.equal("/Library/Fonts/华文黑体.ttf");
-    });
+    if (is("MacOS")) {
+        it("华文黑体的路径是", () => {
+            expect(resolveSystemFontPath("华文黑体")[0]).to.equal("/Library/Fonts/华文黑体.ttf");
+        });
+    }
 
     it("华文黑体应该包含汉字'测试'", () => {
         expect(contain(resolveSystemFontPath("华文黑体")[0], "测试")).to.be.ok;
